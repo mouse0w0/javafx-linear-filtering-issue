c7e698cbfcd963472870e59f1aa43ab08b973797
Mouse <mouse0w0@qq.com>
2024-01-12 22:50:09 +0800

    Init

---
+++ src/main/java/com/mouse0w0/javafxlinearfilteringissue/App.java
@@ -0,0 +1,46 @@
+package com.mouse0w0.javafxlinearfilteringissue;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Point3D;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.Shape3D;
+import javafx.scene.shape.TriangleMesh;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        PhongMaterial material = new PhongMaterial();
+        material.setDiffuseMap(new Image(App.class.getResource("/image.png").openStream()));
+
+        Box box = new Box(200, 200, 200);
+        box.setMaterial(material);
+        box.setTranslateX(250);
+        box.setTranslateY(250);
+        box.setRotate(70);
+        box.setRotationAxis(new Point3D(1,1,1));
+
+        Group root = new Group(box);
+        Scene scene = new Scene(root);
+        scene.setFill(Color.BLACK);
+
+        PerspectiveCamera camera = new PerspectiveCamera();
+        scene.setCamera(camera);
+
+        stage.setScene(scene);
+        stage.setWidth(500);
+        stage.setHeight(500);
+        stage.show();
+    }
+}
---
+++ src/main/java/com/mouse0w0/javafxlinearfilteringissue/Main.java
@@ -0,0 +1,9 @@
+package com.mouse0w0.javafxlinearfilteringissue;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
